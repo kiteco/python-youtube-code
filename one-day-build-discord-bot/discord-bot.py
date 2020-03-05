85efbb24c5c5b17c8bd6f6196cdf19a0fbaa36e2
Brian Rooney <brooney@umass.edu>
2020-03-05 17:35:56 -0500

    Update discord-bot.py

--- one-day-build-discord-bot/discord-bot.py
+++ one-day-build-discord-bot/discord-bot.py
@@ -1,11 +1,9 @@
 import os
 import discord
-from dotenv import load_env
 import asyncio
 import random
-import time
+import datetime
 
-load_env()
 token = os.getenv("DISCORD_API_TOKEN")
 
 bot = discord.Client()
@@ -13,34 +11,43 @@ bot = discord.Client()
 
 @bot.event
 async def on_member_join(member):
-	if member.id = bot.id:
+	if member.id == bot.id:
 		return
-	response = f"Welcome to Kite Fitness, {member.mention}."
-	await bot.send_message(member, response)
+	channel = discord.utils.get(bot.guilds[0].channels, name="general")
+	response = f"Welcome to Kite Fitness, {member.name}."
+	await channel.send(response)
 
 
 @bot.event
 async def on_message(message):
+	print(vars(bot))
+	if message.author == bot.user:
+		return
 	channel = message.channel
 	keywords = ["work", "workout", "push", "push up", "up"]
 	for keyword in keywords:
-		if keyword in message:
-			response = f"Did someone say {keyword}? Drop and give me 20 @{message.author.id}!"
+		if keyword.lower() in message.content.lower():
+			response = f"Did someone say {keyword.lower()}? Drop and give me 10 <@{message.author.id}>!"
 			await channel.send(response)
 
 
-
-@loop(seconds = 3600)
+@bot.event
 async def workout_time():
-	await bot.wait_until_ready()
-	online_members = []
-	for member in bot.get_all_members():
-		if member.status != discord.Status.offline:
-			online_members.append(member.mention)
-	user = random.choice(online_members)
-	time = time.localtime()
-	await bot.send_message(user, f"It's {time}! Time for some push ups {user.name}")
+	while(True):
+		await bot.wait_until_ready()
+		online_members = []
+		for member in bot.get_all_members():
+			if member.status != discord.Status.offline and member.id != bot.user.id:
+				online_members.append(member.id)
+		if len(online_members) > 0:
+			user = random.choice(online_members)
+			current_time = int(datetime.datetime.now().strftime("%I"))
+			channel = discord.utils.get(bot.guilds[0].channels, name="general")
+			message = f"It's {current_time} o'clock! Time for some push ups <@{user}>"
+
+			await channel.send(message)
+		await asyncio.sleep(3600)
 
 
-
+bot.loop.create_task(workout_time())
 bot.run(token)
