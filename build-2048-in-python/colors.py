78bd0733c23d9799d6dfcc8f1e4421202bafcbff
Taylor Carpenter <tacarpe@umich.edu>
2020-03-06 03:51:15 -0500

    Create colors.py

---
+++ build-2048-in-python/colors.py
@@ -0,0 +1,29 @@
+GRID_COLOR = "#a39489"
+EMPTY_CELL_COLOR = "#c2b3a9"
+FONT = ("Helvetica", 24, "bold")
+
+CELL_COLORS = {
+    2: "#fcefe6",
+    4: "#f2e8cb",
+    8: "#f5b682",
+    16: "#f29446",
+    32: "#ff775c",
+    64: "#e64c2e",
+    128: "#ede291",
+    256: "#fce130",
+    1024: "#f0b922",
+    2048: "#fad74d"
+}
+
+CELL_NUMBER_COLORS = {
+    2: "#695c57",
+    4: "#695c57",
+    8: "#ffffff",
+    16: "#ffffff",
+    32: "#ffffff",
+    64: "#ffffff",
+    128: "#ffffff",
+    256: "#ffffff",
+    1024: "#ffffff",
+    2048: "#ffffff"
+}
