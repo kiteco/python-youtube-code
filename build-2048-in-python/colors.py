19f36061a783718390be5e13da686865bc756a4a
Taylor Carpenter <tacarpe@umich.edu>
2020-03-08 18:06:11 -0400

    Update colors.py

--- build-2048-in-python/colors.py
+++ build-2048-in-python/colors.py
@@ -1,6 +1,12 @@
 GRID_COLOR = "#a39489"
 EMPTY_CELL_COLOR = "#c2b3a9"
-FONT = ("Helvetica", 24, "bold")
+NUMBER_FONT = ("Helvetica", 24, "bold")
+SCORE_LABEL_FONT = ("Verdana", 20)
+SCORE_FONT = ("Helvetica", 32, "bold")
+GAME_OVER_FONT = ("Helvetica", 48, "bold")
+GAME_OVER_FONT_COLOR = "#ffffff"
+WINNER_BG = "#ffcc00"
+LOSER_BG = "#a39489"
 
 CELL_COLORS = {
     2: "#fcefe6",
@@ -11,6 +17,7 @@ CELL_COLORS = {
     64: "#e64c2e",
     128: "#ede291",
     256: "#fce130",
+    512: "#ffdb4a",
     1024: "#f0b922",
     2048: "#fad74d"
 }
@@ -24,6 +31,7 @@ CELL_NUMBER_COLORS = {
     64: "#ffffff",
     128: "#ffffff",
     256: "#ffffff",
+    512: "#ffffff",
     1024: "#ffffff",
     2048: "#ffffff"
 }
