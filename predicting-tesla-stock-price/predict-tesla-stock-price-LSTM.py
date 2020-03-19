8a88cfcd77e925e4e71390d058416ce6eb7ac531
Gabe Mersy <gabemersy@gmail.com>
2020-03-19 14:52:49 -0500

    add .py script of LSTM training and testing

---
+++ predicting-tesla-stock-price/predict-tesla-stock-price-LSTM.py
@@ -0,0 +1,161 @@
+# Importing modules
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime
+
+# Reading in the data
+tesla = pd.read_csv('TSLA.csv')
+
+# Isolating the date and close price
+tesla = tesla[['Date', 'Close']]
+
+# Selecting the stationary part of the dataset
+"""
+Start date: 1/1/14 -> index = 884
+End date: 12/31/16 -> index = 1639
+"""
+new_tesla = tesla.loc[884:1639]
+
+# Feature preprocessing
+new_tesla = new_tesla.drop('Date', axis = 1)
+new_tesla = new_tesla.reset_index(drop = True)
+new_tesla.head()
+T = new_tesla.values
+T = T.astype('float32')
+T = np.reshape(T, (-1, 1))
+
+# Min-max scaling to get the values in the range [0,1] to optimize convergence speed
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range = (0, 1))
+T = scaler.fit_transform(T)
+
+# 80-20 split
+train_size = int(len(T) * 0.80)
+test_size = int(len(T) - train_size)
+train, test = T[0:train_size,:], T[train_size:len(T),:]
+
+# Method for converting a time series dataset to a supervised learning problem
+def convert_to_supervised(data, window_size):
+    X, Y = [], []
+    for i in range(len(data) - window_size - 1):
+        window = data[i:(i + window_size), 0]
+        X.append(window)
+        Y.append(data[i + window_size, 0])
+    return np.array(X), np.array(Y)
+
+# Roughly one month of trading assuming 5 trading days per month
+window_size = 20
+X_train, Y_train = convert_to_supervised(train, window_size)
+
+X_test, Y_test = convert_to_supervised(test, window_size)
+
+# Reshape to the format of [samples, time steps, features]
+X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+
+X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+
+# Model imports
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+from keras.callbacks.callbacks import ModelCheckpoint
+
+# Setting seed for reproducibility 
+tf.random.set_seed(11)
+np.random.seed(11)
+
+# Building model
+model = Sequential()
+
+model.add(LSTM(units = 50, activation = 'relu', #return_sequences = True, 
+               input_shape = (X_train.shape[1], window_size)))
+model.add(Dropout(0.2))
+
+# Optional additional model layer to make a deep network. If you want to use this, uncomment #return_sequences param in previous add
+"""
+model.add(LSTM(units = 25, activation = 'relu'))
+model.add(Dropout(0.2))
+"""
+
+# Output layer
+model.add(Dense(1))
+model.compile(loss = 'mean_squared_error', optimizer = 'adam')
+
+# Save models
+filepath = 'saved_models/model_epoch_{epoch:02d}.hdf5'
+
+checkpoint = ModelCheckpoint(filepath = filepath,
+                             monitor = 'val_loss',
+                             verbose = 1,
+                             save_best_only = True,
+                             mode ='min'
+                            )
+
+history = model.fit(X_train, Y_train, epochs = 100, batch_size = 20, validation_data = (X_test, Y_test), 
+                    callbacks = [checkpoint], 
+                    verbose = 1, shuffle = False)
+
+# Load chosen model
+from keras.models import load_model
+
+best_model = load_model('saved_models/model_epoch_89.hdf5')
+
+# Predicting and inverse transforming the predictions
+
+train_predict = best_model.predict(X_train)
+
+Y_hat_train = scaler.inverse_transform(train_predict)
+
+test_predict = best_model.predict(X_test)
+
+Y_hat_test = scaler.inverse_transform(test_predict)
+
+# Inverse transforming the actual values, to return them to their original values
+Y_test = scaler.inverse_transform([Y_test])
+Y_train = scaler.inverse_transform([Y_train])
+
+"""
+Loading the best model and predicting
+"""
+from keras.models import load_model
+
+best_model = load_model('saved_models/model_epoch_89.hdf5')
+
+# Predicting and inverse transforming the predictions
+
+train_predict = best_model.predict(X_train)
+
+Y_hat_train = scaler.inverse_transform(train_predict)
+
+test_predict = best_model.predict(X_test)
+
+Y_hat_test = scaler.inverse_transform(test_predict)
+
+# Inverse transforming the actual values, to return them to their original values
+Y_test = scaler.inverse_transform([Y_test])
+Y_train = scaler.inverse_transform([Y_train])
+
+# Reshaping 
+Y_hat_train = np.reshape(Y_hat_train, newshape = 583)
+Y_hat_test = np.reshape(Y_hat_test, newshape = 131)
+
+Y_train = np.reshape(Y_train, newshape = 583)
+Y_test = np.reshape(Y_test, newshape = 131)
+
+# Model performance evaluation
+from sklearn.metrics import mean_squared_error
+
+train_RMSE = np.sqrt(mean_squared_error(Y_train, Y_hat_train))
+
+test_RMSE = np.sqrt(mean_squared_error(Y_test, Y_hat_test))
+
+print('Train RMSE is: ')
+print(train_RMSE, '\n')
+print('Test RMSE is: ')
+print(test_RMSE)
+
+# For a plot of model performance, see either the plots folder, or the plot at the end of the Jupyter notebook
