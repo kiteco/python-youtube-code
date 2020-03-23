606b7ac238160c7c5cf5383b723adc26ab5a6ab8
Brian Rooney <brooney@umass.edu>
2020-03-23 16:07:39 -0400

    Update slow_speed_twitter_bot.py

--- slow_internet_speed_twitter_bot/slow_speed_twitter_bot.py
+++ slow_internet_speed_twitter_bot/slow_speed_twitter_bot.py
@@ -15,36 +15,30 @@ api = TwitterAPI(consumer_key,
                  access_token_secret)
 
 
-def check_speeds(checks, download_total, upload_total):
+def check_speeds():
 	speedtester = speedtest.Speedtest()
+
+	advertised_download = 60
+	advertised_upload = 2
 
 	# returned as bits, converted to megabits
 	download_speed = int(speedtester.download() / 1000000)
 	upload_speed = int(speedtester.upload() / 1000000)
 
-	download_total += download_speed
-	upload_total += upload_speed
-	checks += 1
-
-	upload_avg = upload_total / checks
-	download_avg = download_total / checks
-
 	print("Download Speed: " + str(download_speed) + "Mbps")
 	print("Upload Speed: " + str(upload_speed) + "Mbps")
-	print("Download Average: " + str(download_avg) + "Mbps")
-	print("Upload Average: " + str(upload_avg) + "Mbps")
 	print("\n")
 
-	if download_speed < download_avg * 0.75 or upload_speed < upload_avg * 0.75:
-		tweet = "@XFinity My speeds are 25% slower than usual, any idea why? 😊"
+	if download_speed < advertised_download * 0.8 or upload_speed < advertised_upload * 0.8:
+		tweet = "@XFinity My speeds are 20% slower than usual, any idea why? 😊"
 		api.request("statuses/update", {"status": tweet})
 
-	if download_speed < download_avg * 0.5 or upload_speed < upload_avg * 0.5:
+	if download_speed < advertised_download * 0.5 or upload_speed < advertised_upload * 0.5:
 		tweet = "@XFinity My speeds are 50 PERCENT slower than averge... Please fix this. 😒"
 		api.request("statuses/update", {"status": tweet})
 
-	if download_speed < download_avg * 0.2 or upload_speed < upload_avg * 0.2:
-		tweet = "Using my phone as a hotspot because my @XFinity speeds are 80% slower than usual 😠"
+	if download_speed < advertised_download * 0.25 or upload_speed < advertised_upload * 0.25:
+		tweet = "Using my phone as a hotspot because my @XFinity speeds are 75% slower than usual 😠"
 		api.request("statuses/update", {"status": tweet})
 
 
@@ -52,8 +46,8 @@ scheduler = sched.scheduler(time.time, time.sleep)
 
 def periodic_check(scheduler, interval, action, arguments=()):
 	scheduler.enter(interval, 1, periodic_check, (scheduler, interval, action, arguments))
-	action(arguments[0], arguments[1], arguments[2])
+	action()
 
 
-periodic_check(scheduler, 1, check_speeds, (1, 60.0, 2.0))
+periodic_check(scheduler, 1, check_speeds)
 scheduler.run()
