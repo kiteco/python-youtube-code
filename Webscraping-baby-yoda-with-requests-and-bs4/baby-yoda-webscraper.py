4899ef87e51783515a42410b5004f506a14767d3
caelan-kite <45773958+caelan-kite@users.noreply.github.com>
2020-06-12 11:25:18 -0700

    Updating with bs4 improvements and comments for clarification

--- Webscraping-baby-yoda-with-requests-and-bs4/baby-yoda-webscraper.py
+++ Webscraping-baby-yoda-with-requests-and-bs4/baby-yoda-webscraper.py
@@ -2,13 +2,25 @@ from bs4 import BeautifulSoup
 import urllib.request
 import requests
 
-url = "http://www.reddit.com/r/BabyYoda"
+# setting URL destination
+url = "https://www.reddit.com/r/BabyYoda"
+
+# retrieving HTML payload from the website
 response = requests.get(url)
+
+# checking response.status_code (if you get 502, try rerunning the code)
+print(f'Status: {response.status_code} (should be 200)\n')
+
+# using BeautifulSoup to parse the response object
 soup = BeautifulSoup(response.content, "html.parser")
+
+# finding Post images in the soup
 images = soup.find_all("img", attrs={"alt":"Post image"})
+
+# downloading images
 number = 0
 for image in images:
-	print(image["src"])
-	image_src = image["src"]
-	urllib.request.urlretrieve(image_src, str(number))
-	number += 1
+    print(image["src"])
+    image_src = image["src"]
+    urllib.request.urlretrieve(image_src, str(number))
+    number += 1
